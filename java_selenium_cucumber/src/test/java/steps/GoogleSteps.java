1edc57bc10d9d9de7862122e76a171fdb8627986
jorgedediosb <jorgededios@icloud.com>
2023-09-26 18:27:38 +0200

    add BasePage.java & GooglePage.java

--- java_selenium_cucumber/src/test/java/steps/GoogleSteps.java
+++ java_selenium_cucumber/src/test/java/steps/GoogleSteps.java
@@ -2,12 +2,16 @@ package steps;
 
 // importamos 'Given', 'When', 'Then', etc. El '*' hace que importe todos de una vez
 import cucumber.api.java.en.*;
+import pages.GooglePage;
 
 public class GoogleSteps {
+
+    GooglePage google = new GooglePage();
+
     // con ^ y $ decimos a cucumber que este paso es "sólo esto"
     @Given("^I am on the Google search page$")
-    public void navigateTogoogle(){
-
+    public void navigateToGoogle(){
+    google.navigateToGoogle();
     }
 
     @When("^I enter a search criteria$")
